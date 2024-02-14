b5e3cff4df2e0c2718a79cb3cbfd951b6e0daaf8
Rishu Raj <131179273+Rishut681@users.noreply.github.com>
2024-02-14 15:13:19 +0530

    navbar ready

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Navbar from "./Navbar";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <MainHeader>
+      <NavLink to="/">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtid1RQ_CRYPxcA7hP30eSg7I0BCt44k4IeQ&usqp=CAU" alt="logo" className="logo" />
+      </NavLink>
+      <Navbar />
+    </MainHeader>
+  );
+};
+
+const MainHeader = styled.header`
+  padding: 0 4.8rem;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    height: auto;
+    max-width: 30%;
+  }
+`;
+
+export default Header;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-vg21ui1e28eb6ltg.us.auth0.com"
+    clientId="vZxV6JMCMBcQEGGCqyg6KTGLiAJpF8gg"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>
 )
